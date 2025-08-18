80ce5cc62cf6785a3f0e8de252ddd9bcff78d023
SuanEden <silveirsuan@gmail.com>
2025-08-18 15:47:20 -0300

    Cria Função coletaDados

---
+++ funcoes.py
@@ -0,0 +1,11 @@
+import sys
+def coletaDados():
+    """
+    Coleta as informações da demanda
+    """
+    titulo = input('Qual a demanda? ')
+    data = input('Qual a Data da demanda? ')
+    descricao = input('Descrição da Demanda: ')
+
+
+
