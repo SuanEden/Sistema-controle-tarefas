10aa3c2ca58aa0e7860d777501f0ba1ee6846f07
SuanEden <silveirsuan@gmail.com>
2025-08-18 15:47:51 -0300

    Cria Função altenticacaoUsuario

--- funcoes.py
+++ funcoes.py
@@ -8,4 +8,28 @@ def coletaDados():
     descricao = input('Descrição da Demanda: ')
 
 
+def altenticacaoUsuario():
+    usuario_resistrado = 'Suan'
+    senha_resistrada = '1831'
+    limite_tentativas = 2
+    tentativa = 0
+    usuario = input('Qual o nome do Usuario:')
+    while(usuario_resistrado != usuario):
+        usuario = input('Usuario incorreto, tente novamente \nnome do Usuario:')
+        tentativa+=1
+        
+        if(tentativa>=limite_tentativas):
+            print('Você exedeu o limete de tentativas')
+            sys.exit(f'O sistema se encerrou por exeder o limite de tentativas')
+
+    senha = input(f'Olá {usuario}, qual a sua senha: ')
+    while(senha_resistrada != senha):
+        senha = input('Senha incorreta, tente novamente \nQual a sua Senha:')
+        tentativa+=1
+        
+        if(tentativa>=limite_tentativas):
+            print('Você exedeu o limete de tentativas')
+            sys.exit(f'O sistema se encerrou por exeder o limite de tentativas')
+    print(f'Seja bem vindo {usuario}, como está?')
+
 
