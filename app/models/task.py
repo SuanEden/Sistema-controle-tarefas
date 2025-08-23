9ae2d1b2c98865cb99371678e6d6b7ea823f3100
SuanEden <silveirasuan@gmail.com>
2025-08-23 16:40:11 -0300

    Cria tabela do SQL

--- app/models/task.py
+++ app/models/task.py
@@ -7,3 +7,21 @@ status -> string, valores possiveis: todo, soing, done
 created_at -> datetime, gerado automaticamente
 upgrated_at -> datetime, gerado automaticamente
 """
+from sqlmodel import SQLModel, Field
+from datetime import datetime
+from sqlalchemy import ForeignKey
+from emun import Enum
+
+class Status(str, Enum):
+    todo = "todo"
+    doing = "doing"
+    done = "done"
+
+class Task(SQLModel, table = True):
+    id_demanda: int = Field(primary_key = True, default = None)
+    user_id: int = Field(..., foreign_key = "user.id")
+    titulo: str = Field(..., max_length = 120, description = "Titulo da tarefa" )
+    descricao: str = Field(default = None, nullable = True, description = "Descrição de terefa")
+    due_date: datetime = Field(default_factory = datetime.utcnow, description = "Data de vencimentp da tarefa")
+    status: Status = Field(..., default = Status.todo, description = "Status da tarefa: todo, doing ou done")
+    updated_at: datetime = Field(default_factory  = datetime.utcnow, description = "Ultima atualização")
